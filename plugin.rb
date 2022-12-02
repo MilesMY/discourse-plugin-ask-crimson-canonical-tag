0c5c041a044e5a9248a3b8aceb44764a9960571a
Miles Yu <m.yu@crimsoneducation.org>
2022-12-02 06:46:43 +0000

    Fix error by adding metadata

--- plugin.rb
+++ plugin.rb
@@ -1,3 +1,10 @@
+# name: discourse-plugin-ask-crimson-canonical-tag
+# about: crimson plugin to rewrite canonical url 
+# version: 0.0.1
+# date: 2 Dec 2022
+# authors: MilesMY
+# url: https://github.com/MilesMY/discourse-plugin-ask-crimson-canonical-tag
+
 after_initialize do
   module CanonicalURL::Helpers
     def canonical_link_tag(url = nil)
